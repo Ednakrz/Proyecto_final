5eb72151dde0bd8ace77a1ef9e1367e011a1e7df
Gabriel-GV <gabrielg@lcg.unam.mx>
2024-06-05 16:01:07 -0600

    Módulo para transcribir.

--- dna_rna_analysis/operations/dna_to_rna.py
+++ dna_rna_analysis/operations/dna_to_rna.py
@@ -1,4 +1,15 @@
 def transcribe_dna_to_rna(dna_sequence):
-    """Transcribe una secuencia de ADN a ARN."""
-    return dna_sequence.replace('T', 'U')
+    """
+    Transcribe una secuencia de ADN a ARN.
 
+    Este método reemplaza todas las apariciones de la base nitrogenada timina (T)
+    en la secuencia de ADN por uracilo (U) para obtener la secuencia de ARN.
+
+    Parameters:
+    dna_sequence (str): Una cadena de caracteres que representa la secuencia de ADN.
+
+    Returns:
+    str: La secuencia de ARN resultante de la transcripción del ADN.
+    """
+    rna_sequence = dna_sequence.replace('T', 'U')
+    return rna_sequence
