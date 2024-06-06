32d888a0d06334c47a3d7ed93c3ecd3f01b5a29f
Gabriel-GV <gabrielg@lcg.unam.mx>
2024-06-05 18:46:11 -0600

    Documentación añadida

--- dna_rna_analysis/operations/mRNA_proteins.py
+++ dna_rna_analysis/operations/mRNA_proteins.py
@@ -1,7 +1,22 @@
-from Bio.Seq import Seq
 from Bio.SeqUtils import translate
 
 def secuencia_de_proteinas(mrna_sequence):
+    """
+    Traduce una secuencia de ARN mensajero (ARNm) en una secuencia de aminoácidos.
+
+    Parameters:
+    mrna_sequence (str): La secuencia de ARN mensajero a traducir.
+
+    Returns:
+    str: La secuencia de aminoácidos traducida.
+
+    Raises:
+    None
+
+    Esta función itera sobre la secuencia de ARNm en pasos de tres nucleótidos,
+    traduciendo cada codón en un aminoácido utilizando la función 'translate' de BioPython.
+    La secuencia de aminoácidos resultante se acumula y se devuelve al final de la función.
+    """
     protein_sequence = ""
     for i in range(0, len(mrna_sequence), 3):
         codon = mrna_sequence[i:i+3]
@@ -10,5 +25,6 @@ def secuencia_de_proteinas(mrna_sequence):
         amino_acid = translate(codon)
         protein_sequence += amino_acid
     return protein_sequence
+
 
 
