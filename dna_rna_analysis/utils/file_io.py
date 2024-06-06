22bb451c681268542c5bbdfc0cb913de1a279adf
Gabriel-GV <gabrielg@lcg.unam.mx>
2024-06-05 21:45:44 -0600

    Archivos para pruebas.

--- dna_rna_analysis/utils/file_io.py
+++ dna_rna_analysis/utils/file_io.py
@@ -8,7 +8,6 @@ Funciones:
     - read_dna_sequence: Lee una secuencia de ADN de un archivo de texto.
 """
 
-
 def read_dna_sequence(filename):
     """
     Lee una secuencia de ADN desde un archivo de texto.
@@ -23,10 +22,16 @@ def read_dna_sequence(filename):
         FileNotFoundError: Si el archivo especificado no se encuentra.
         ValueError: Si el archivo está vacío o contiene caracteres no válidos.
     """
-    with open(filename, 'r') as file:
-        sequence = file.read().strip().upper()
+    try:
+        with open(filename, 'r') as file:
+            sequence = file.read().strip()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"El archivo '{filename}' no se encuentra.")
+
     if not sequence:
         raise ValueError("El archivo está vacío.")
-    if any(char not in 'ACGT' for char in sequence):
+    
+    if any(char not in 'ACGTacgt' for char in sequence):
         raise ValueError("La secuencia contiene caracteres no válidos.")
-    return sequence
+    
+    return sequence.upper()
