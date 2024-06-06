7ebd58a18b664f79679752644c8c0ffc31ef4202
Gabriel-GV <gabrielg@lcg.unam.mx>
2024-06-05 22:06:32 -0600

    Corrección en la conversión a RNA.

--- dna_rna_analysis/operations/dna_to_rna.py
+++ dna_rna_analysis/operations/dna_to_rna.py
@@ -2,8 +2,11 @@ def transcribe_dna_to_rna(dna_sequence):
     """
     Transcribe una secuencia de ADN a ARN.
 
-    Este método reemplaza todas las apariciones de la base nitrogenada timina (T)
-    en la secuencia de ADN por uracilo (U) para obtener la secuencia de ARN.
+    Este método reemplaza las bases nitrogenadas de acuerdo a las siguientes reglas:
+    - Adenina (A) se reemplaza por uracilo (U).
+    - Citosina (C) se reemplaza por guanina (G).
+    - Guanina (G) se reemplaza por citosina (C).
+    - Timina (T) se reemplaza por adenina (A).
 
     Parameters:
     dna_sequence (str): Una cadena de caracteres que representa la secuencia de ADN.
@@ -11,5 +14,12 @@ def transcribe_dna_to_rna(dna_sequence):
     Returns:
     str: La secuencia de ARN resultante de la transcripción del ADN.
     """
-    rna_sequence = dna_sequence.replace('T', 'U')
-    return rna_sequence
+    # Reemplazar todas las bases de acuerdo a las reglas especificadas
+    rna_sequence = dna_sequence.replace('A', 'u').replace('C', 'g').replace('G', 'c').replace('T', 'a')
+    return rna_sequence.upper()
+
+# Ejemplo de uso
+dna_sequence = "ATCG"
+rna_sequence = transcribe_dna_to_rna(dna_sequence)
+print(rna_sequence)  # Debe imprimir "UAGC"
+
