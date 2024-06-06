fa06c2eef1d3aac8b5bd5c65c473391a2aeee619
Gabriel-GV <gabrielg@lcg.unam.mx>
2024-06-05 18:35:42 -0600

    Se actualiza función con biblioteca de biopython

--- dna_rna_analysis/operations/mRNA_proteins.py
+++ dna_rna_analysis/operations/mRNA_proteins.py
@@ -1,8 +1,14 @@
 from Bio.Seq import Seq
+from Bio.SeqUtils import translate
 
-def secuencia_de_proteinas (mrna_secuence): 
-    seq_protein = mrna_secuence.translate()
-
-    print (seq_protein)
+def secuencia_de_proteinas(mrna_sequence):
+    protein_sequence = ""
+    for i in range(0, len(mrna_sequence), 3):
+        codon = mrna_sequence[i:i+3]
+        if len(codon) != 3:
+            break
+        amino_acid = translate(codon)
+        protein_sequence += amino_acid
+    print("Secuencia de aminoácidos:", protein_sequence)
 
 
